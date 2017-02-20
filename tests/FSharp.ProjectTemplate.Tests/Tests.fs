80d265160a50571480a6b3f4a3589ec707083a32
Robin Munn <rmunn@pobox.com>
2017-02-20 21:44:33 +0700

    Use Expecto instead of NUnit for unit testing
    This also brings in dozens of dependencies and makes the paket.lock file
    look huge, but that's because running "paket update" now pulls in .Net
    Core builds of FSharp.Core.

--- tests/FSharp.ProjectTemplate.Tests/Tests.fs
+++ tests/FSharp.ProjectTemplate.Tests/Tests.fs
@@ -1,10 +1,19 @@
 ﻿module FSharp.ProjectScaffold.Tests
 
 open FSharp.ProjectTemplate
-open NUnit.Framework
+open Expecto
 
-[<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+[<Tests>]
+let tests =
+    testList "Library tests" [
+        testCase "hello returns 42" <| fun _ ->
+            let result = Library.hello 42
+            Expect.equal result 42 "Result should have been 42"
+        testProperty "In fact, hello returns 42 no matter what you pass it" <| fun x ->
+            let result = Library.hello x
+            Expect.equal result 42 "Result should always be 42 no matter what the parameter is"
+    ]
+
+[<EntryPoint>]
+let main args =
+    runTestsInAssembly defaultConfig args
